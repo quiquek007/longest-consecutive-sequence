5b2f7252e80d6a781d73900e05cbcb81a10a1f99
quiquek007 <quiquek007@gmail.com>
2018-09-24 18:04:57 +0300

    Solution: Increase Perfomance and errors(18s)

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ module.exports = function longestConsecutiveLength(array) {
       maxLength = countBig + countSmall + 1;
     }
     if (maxLength < 2) i += maxLength - 1;
-    else i += maxLength;
+    else i += maxLength + 1;
     array.splice(0,1);
   }
 
